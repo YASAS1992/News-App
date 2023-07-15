fddbe2af41c2affd85581ed4657fbd10529579ad
Yasas Weerasekara <>
2023-07-15 10:55:13 +0530

    bottom navigayion

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.newsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation(){
+        val bottomNavigation = binding.btmNav
+        var navController = Navigation.findNavController(this,R.id.host_fragment)
+        NavigationUI.setupWithNavController(bottomNavigation,navController)
     }
 }
